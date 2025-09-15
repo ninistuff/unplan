21fb90d0bb69655104d74ee9e13fb3110ec07df0
ninistuff <230558428+ninistuff@users.noreply.github.com>
2025-09-15 12:09:48 +0300

    router: set /home entry, add home screen

---
+++ app/home.tsx
@@ -0,0 +1,11 @@
+import { router } from 'expo-router'
+import { Button, Text, View } from 'react-native'
+
+export default function Home() {
+return (
+<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 12 }}>
+<Text>Home</Text>
+<Button title="Mergi la Results" onPress={() => router.push('/results')} />
+</View>
+)
+}
